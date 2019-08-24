16b3d8f0056b80b9dac17fb6b5038af8a0279184
Flávio José <flavioj2008@gmail.com>
2019-08-24 10:13:37 -0300

    Criando operacao de instanciacao

---
+++ src/main/java/com/fjstecnologia/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.fjstecnologia.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fjstecnologia.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+}
